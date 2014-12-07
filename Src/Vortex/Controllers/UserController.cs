19440d0443a138c60dbc518339aab62105e98860
mzorec <mzorec@gmail.com>
2014-12-07 16:15:16 +0100

    vortex solution

---
+++ Src/Vortex/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vortex.Models;
+
+namespace Vortex.Controllers
+{
+    public class UserController : ApiController
+    {
+        public IHttpActionResult Register(User user)
+        {
+            return new NotImplementedException()
+        }
+
+        public IHttpActionResult Login(string userName, string password)
+        {
+            return Ok();
+        }
+    }
+}
