4de67217bc78dc86caf0759999adeafe1f5b8820
iM4tt <imattdev@gmail.com>
2014-12-09 13:36:53 +0100

    task #1

---
+++ Src/Vortex.Tests/Learning/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Vortex.Tests.Learning
+{
+    class CalculatorTests 
+    {
+
+        [Test]
+        public void Zracune()
+        {
+           ICalculator calc = new Calculator();
+            int result = calc.izracunaj(1, 2);
+            Assert.AreEqual(3, result);
+        }
+       
+    }
+}
--- Src/Vortex.Tests/Learning/Class1.cs
+++ Src/Vortex.Tests/Learning/Class1.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 using System.Threading.Tasks;
 
 namespace Vortex.Tests.Learning
 {
-    class Class1
+
+    public interface ICalculator
     {
+        int izracunaj(int a, int b);
+    }
+
+    public class Calculator : ICalculator
+    {
+        public int izracunaj(int a, int b)
+        {
+            int rezultat = a + b;
+            return rezultat;
+        }
     }
 }
