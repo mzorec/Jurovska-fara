1b420147f2c698ff9b76c1527761ce75bfa42768
iM4tt <imattdev@gmail.com>
2014-12-09 18:22:36 +0100

    Task #2

---
+++ Src/Vortex.Tests/Learning/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Vortex.Tests.Learning
+{
+
+
+    public class Calculator : ICalculator
+    {
+
+        public int Calculate(string a)
+        {
+
+            if (a.Contains("+"))
+            {
+                string[] delimiter = new string[] { "+" };
+                var content = a.Split(delimiter, StringSplitOptions.None);
+                var result = 0;
+                for (int i = 0; i < content.Length; i++)
+                {
+                    result = result + int.Parse(content[i]);
+                }
+                //string firstt = content[0];
+                //string secondt = content[1];
+                //int first = int.Parse(firstt);
+                //int second = int.Parse(secondt);
+                //int result = first + second;
+                return result;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+
+    }
+}
---
+++ Src/Vortex.Tests/Learning/iCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vortex.Tests.Learning
+{
+    public interface ICalculator
+    {
+        int Calculate(string a);
+    }
+}
